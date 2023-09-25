dd6d5255e1cffcfbc28b9912fbd52b4505417af4
David Dinh <dndinh7@gmail.com>
2023-09-24 23:27:58 -0400

    set up the initial display window in SDL, need to see how to respond to key events

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,31 @@
 Chip8 chip8;
 
 int main() {
+  const int WIDTH= 64;
+  const int HEIGHT= 32;
+  const int SCALE= 20;
+
+  // init SDL
+  if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+    std::cout << "Error initializing SDL" << std::endl;
+    return 1;
+  }
+  
+  SDL_Window* win= SDL_CreateWindow("Game Canvas",
+                                     SDL_WINDOWPOS_CENTERED,
+                                     SDL_WINDOWPOS_CENTERED,
+                                     WIDTH * SCALE, HEIGHT * SCALE, 
+                                     SDL_WINDOW_SHOWN);
+
+  if (win == nullptr) {
+    std::cout << "Could not initialize window" << std::endl;
+    return 1;
+  }
+
+  SDL_Delay(3000);
+
+  SDL_DestroyWindow(win);
+  SDL_Quit();
 
   return 0;
 
